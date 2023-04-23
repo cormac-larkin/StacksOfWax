c24b2dc86c0be0b9635a945b829d69e6d029b5f2
Cormac Larkin <larkincormac@gmail.com>
2023-04-23 15:26:54 +0100

    Added 'edit review' feature

--- routes/reviews.js
+++ routes/reviews.js
@@ -49,4 +49,61 @@ router.post("/delete", ensureAuthenticated, (req, res) => {
   });
 });
 
+router.get("/edit", ensureAuthenticated, (req, res) => {
+
+    const reviewId = req.query.id;
+
+    // Return 404 if no reviewId was provided in the query params
+    if(!reviewId) {
+      return res.redirect("/error/404");
+    }
+
+    // Check that the request is coming from the author of the review
+    db.query("SELECT * FROM review WHERE review_id = ?", [reviewId], (err, result) => {
+      if (err) {
+        console.error(err);
+        return res.redirect("/error/500");
+      }
+      const reviewData = result[0];
+      const ownerId = reviewData.user_id;
+  
+      // Return '403 Forbidden' response if the request is not coming from the review's author
+      if (req.session.user.id != ownerId) {
+        return res.status(403).send();
+      }
+
+      res.render("edit_review", {user: req.session.user, reviewData})
+    });   
+});
+
+router.post("/edit", ensureAuthenticated, (req, res) => {
+
+  const {reviewId, newTitle, newText, newRating} = req.body;
+
+    // Check that the request is coming from the author of the review
+    db.query("SELECT user_id FROM review WHERE review_id = ?", [reviewId], (err, result) => {
+      if (err) {
+        console.error(err);
+        return res.redirect("/error/500");
+      } 
+      const ownerId = result[0].user_id;
+  
+      // Return '403 Forbidden' response if the request is not coming from the review's author
+      if (req.session.user.id != ownerId) {
+        return res.status(403).send();
+      }
+
+      // Otherwise update the review and redirect back to the User's account page
+      db.query("UPDATE review SET title = ?, text = ?, rating = ?, timestamp = ? WHERE review_id = ?", [newTitle, newText, newRating, new Date(), reviewId], (err, result) => {
+        if (err) {
+          console.error(err);
+          return res.redirect("/error/500");
+        }
+        req.flash("success", "Review edited successfully!");
+        res.redirect(`/users?id=${req.session.user.id}`);
+      });
+
+    })
+});
+
 module.exports = router;
